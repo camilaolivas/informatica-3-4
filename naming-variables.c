4d67905fdc1f3fb00b9892ad2e004b7f808b1533
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-04-21 21:48:35 +0000

    we fixed names on the variables

---
+++ naming-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int number;
+    //We delteted the exclamation sign
+    int test_value;
+    //We used and underscore
+    int example1;
+    //We changed the number so the sentence start with a letter
+    float maria;
+    //We deleted int and add the name of the variable
+    printf("Variables created successfully.\n");./.
+}
