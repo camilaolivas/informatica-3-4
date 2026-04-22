cdda63522bb97bad18b0dbc3634faeb55afb39ca
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-04-22 17:41:58 +0000

    Coconut Cranberry Dessert recipe finished

---
+++ recipe.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Coconut Cranberry Dessert\n");
+
+    printf("\n");
+
+    printf("1 1/2 cups \tgraham cracker crumbs\n1/2 cup \tbutter or margarine, melted\n1 can \tsweetened condensed milk\n1 cup \tflaked coconut\n1 cup \tdried cranberries\n2 cups \tvanilla chips\n1 cup \tsliced almonds or pecans");
+
+    printf("\n");
+
+    printf("1.Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ pan.\n");
+
+    printf("\n");
+
+    printf("2.Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
+
+    printf("\n");
+}
