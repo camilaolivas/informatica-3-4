a9d0637ef8d4c0a80baeaa07f09bb4099dc1d940
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-04-23 18:09:30 +0000

    vending2 no terminado

--- vending2.c
+++ vending2.c
@@ -13,47 +13,7 @@ int main (void)
 
   printf("-----------------------------.\n");
 
-  printf("Item\tPrice\tQuantity\tCode.\n");
+  printf("Item\tPrice\tQuantity\tCode\n");
 
-  printf("Snikers \t%.2f.\n\t%d.\n\t%c.\n",price1,quantity1,code1);
-
-  printf(" %d.\n\t",quantity1);
-
-  printf("Selection Code: %c.\n\t",code1);
-
-  //Create variables for second snack (Doritos)
-
-  float price2 = 25.60;
-
-  int quantity2 = 15;
-
-  char code2 = 'd';
-
-  printf("\n");
-
-   printf("Item name: Doritos.\n");
-
-   printf("Price: $%.2f.\n",price2);
-
-  printf("Quantity Available: %d.\n",quantity2);
-
-  printf("Selection Code: %c.\n",code2);
-
-  //Create variables for third snack (Dr.Pepper)
-
-   float price3 = 28.99;
-
-  int quantity3 = 21;
-
-  char code3 = 'p';
-
- printf("\n");
-
-   printf("Item name: Dr.Pepper.\n");
-
-   printf("Price: $%.2f.\n",price3);
-
-  printf("Quantity Available: %d.\n",quantity3);
-
-  printf("Selection Code: %c.\n",code3);
+  printf("Snikers$%.2f%d%c",price1,quantity1,code1);
 }
