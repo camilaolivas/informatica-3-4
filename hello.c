5750523e7f9eaca87c1494837207f214018541aa
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-04-24 14:48:57 +0000

    we learned how to print a response from the user (we actually asked for it) WE'RE TURNING THIS INTO A REAL COMPUTER!

--- hello.c
+++ hello.c
@@ -2,5 +2,20 @@
 
 int main (void)
 {
-    printf("Hello Camila! \n");
+    char urname[50];
+
+    printf("What's your name?: ");
+
+    scanf("%s", &urname);
+
+    printf("Hello %s! \n",urname);
+
+    char favcolor[50];
+
+    printf("What's your favorite color sweetheart?: ");
+
+    scanf("%s", &favcolor);
+
+    printf("Your favorite color is %s! \n",favcolor);
+
 }
