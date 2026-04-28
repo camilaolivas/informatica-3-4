12102da539c45224e2f84f79bb299b7e657a2a0d
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-04-28 21:58:58 +0000

    minutes sin terminar y con errores

---
+++ minutes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (void)
+{
+   int mvrn;
+   int strtm;
+
+   printf("Movie running time:");
+
+   scanf("%d", &mvrn);
+
+    printf("Start time:");
+
+    scanf("%d", &strtm);
+
+    int hinm = mvrn/60
+    int mit = hinm %1;
+    int edtm =strtm-hinm-mit;
+    int endt =
+
+    printf("The movie is %d hr long:\n",hinm );
+    printf("%d min long\n", mit);
+    printf("End time is %d hr \n", endtm);
+    printf("and %d min \n", endt);
+}
