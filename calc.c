d4b3880166b475d24e3026eebaa68cb49c2961aa
camilaolivas <wjpngqhyy9@privaterelay.appleid.com>
2026-05-12 21:49:10 +0000

    haven't solve the 0 thing

--- calc.c
+++ calc.c
@@ -23,42 +23,41 @@ int main(void){
         multiplication();}
         else if (user_response== 4){
         division();}
-    else {
-        printf("Invalid Option\n");
+        else {
+        printf("Invalid option\n");
     }
 
 }
-
 //2.Definition
 void addition(void){
     float a;
     float b;
-    printf("What did you wanna add");
-    scanf("%f %f",&a, &b);
+    printf("What did you wanna add?\n");
+    scanf("%f %f",&a,&b);
     float r = a + b;
     printf("%f + %f = %f\n", a, b, r);
 }
-void subtraction(void){
+ void subtraction(void){
     float a2;
     float b2;
-    printf("What did you wanna subtract");
-    scanf("%f %f",&a2, &b2);
-    float r2 = a2 + b2;
+    printf("What did you wanna subtract?\n");
+    scanf("%f %f",&a2,&b2);
+    float r2 = a2 - b2;
     printf("%f - %f = %f\n", a2, b2, r2);
-}
+ }
 void multiplication(void){
     float a3;
     float b3;
-    printf("What did you wanna multiply");
-    scanf("%f %f",&a3, &b3);
-    float r3 = a3 + b3;
-    printf("%f + %f = %f\n", a3, b3, r3);
+    printf("What did you wanna multiply?\n");
+    scanf("%f %f",&a3,&b3);
+    float r3 = a3 * b3;
+    printf("%f * %f = %f\n", a3, b3, r3);
 }
 void division(void){
     float a4;
     float b4;
-    printf("What did you wanna divide");
-    scanf("%f %f",&a4, &b4);
-    float r4 = a4 + b4;
-    printf("%f + %f = %f\n", a4, b4, r4);
+    printf("What did you wanna divide?\n");
+    scanf("%f %f",&a4,&b4);
+    float r4 = a4 / b4;
+    printf("%f / %f = %f\n", a4, b4, r4);
 }
